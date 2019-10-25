495be3fa7d6141a5ca17566464a1ed49273605b6
Glitch (1fcc-file-metadata-dc) <none>
2019-10-25 12:59:07 +0000

    🌑🐽 Updated with Glitch

--- server.js
+++ server.js
@@ -1,55 +1,45 @@
-'use strict';
-// server.js
-// where your node app starts
+"use strict";
 
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-var bodyParser=require('body-parser');
-var dns = require('dns');
-var multer = require('multer');
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 }));
+app.use(express.static("public"));
+var bodyParser = require("body-parser");
+var dns = require("dns");
+var multer = require("multer");
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-var upload = multer().single('upfile')
-
-
-
+var upload = multer().single("upfile");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/index.html");
 });
 app.post("/upload", function(req, res) {
-  upload(req, res, function (err) {
+  upload(req, res, function(err) {
     if (err) {
       console.log(err);
     }
     let fileName = req.file.originalname;
-  let fileSize = req.file.size
-  let fileType = req.file.mimetype;
-  let fieldName = req.file.fieldname;
-  
-  res.json({"Field Name": fieldName ,"File Name": fileName, "File Size": fileSize, "File Type": fileType})
-  
-  })
-})
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+    let fileSize = req.file.size;
+    let fileType = req.file.mimetype;
+    let fieldName = req.file.fieldname;
+
+    res.json({
+      "Field Name": fieldName,
+      "File Name": fileName,
+      "File Size": fileSize,
+      "File Type": fileType
+    });
+  });
 });
-
-
-
+app.listen(port, function() {
+  console.log("Node.js listening ...");
+});
